8ce6ba705cd79173849589d19f86dfa36d0ce990
chepceline <chepceline25@gmail.com>
2022-04-09 14:19:07 +0300

    initial

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes: Quote[] = [
+    new Quote(1, 'First Quote', 0, 0 ),
+    new Quote(2, 'Second Quote', 0, 0 ),
+    new Quote(1, 'Third Quote', 0, 0 ),
+  ]
+    
   constructor() { }
 
   ngOnInit(): void {
