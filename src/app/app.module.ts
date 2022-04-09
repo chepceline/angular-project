6032406aa0c89ff09eba959ca974d08628265f0d
chepceline <chepceline25@gmail.com>
2022-04-09 15:19:08 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    QuotesComponent
+    QuotesComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
