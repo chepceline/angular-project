f72cb2cba53adcd9a1c6c1bf1851626c9f26c084
chepceline <chepceline25@gmail.com>
2022-04-11 17:59:47 +0300

    include changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 import { QuotesComponent } from './quotes/quotes.component';
 import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+    
     QuotesComponent,
     HighlightDirective
   ],
